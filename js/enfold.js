306df7a22784e999710800796e495b8784415520
25rae25 <rae0024@naver.com>
2020-05-22 13:07:58 +0900

    scroll

--- js/enfold.js
+++ js/enfold.js
@@ -1,22 +1,51 @@
-/************* 사전 지식 *****************/
+/*********** 사전지식 ***********/
 
 
-
-
-/************* 전역변수 *****************/
+/*********** 전역변수 ***********/
+var scTop = 0;
 var isWingShow = false;
 
+var $mainSlide = $(".main-wrap .slide");
+var mainNow = 0;
+var mainLast = $mainSlide.length - 1;
+var mainSpeed = 500;
+var mainGap = 3000;
+var mainInterval;
+var mainPager = {off: '○', on: '●'};
+mainInit();
+mainPagerInit();
+onMainLeave();
+
+/*********** 사용자정의 ***********/
+function mainInit() {
+	$(".main-wrap .slides").empty();
+	$($mainSlide[mainNow]).appendTo(".main-wrap .slides").removeClass("slide");
+}
+
+function mainPagerInit() {
+	for(var i=0, html; i<=mainLast; i++) {
+		if(mainNow == i) html = '<span class="pager">'+mainPager.on+'</span>';
+		else html = '<span class="pager">'+mainPager.off+'</span>';
+		$(html).appendTo(".main-wrap .pagers").click(onMainPager);
+	}
+}
+
+function mainAni() {
+	$(".main-wrap .pager").html(mainPager.off);
+	$(".main-wrap .pager").eq(mainNow).html(mainPager.on);
+	$($mainSlide[mainNow]).prependTo(".main-wrap .slides")
+	.css("opacity", 0)
+	.addClass("slide")
+	.stop()
+	.animate({"opacity": 1}, mainSpeed, mainInit);
+}
 
 
-/************* 사용자정의 *****************/
-
-
-
-/************* 이벤트콜백 *****************/
+/*********** 이벤트콜백 ***********/
 function onWingClick() {
 	if(isWingShow) {
 		isWingShow = false;
-		$(".wing-wrap").css("background-color", "rgba(0,0,0,0)");
+		$(".wing-wrap").css("background-color", "rgba(0, 0, 0, 0)");
 		$(".wing-conts").css("transform", "translateX(350px)");
 		setTimeout(function(){
 			$(".wing-wrap").css("display", "none");
@@ -26,7 +55,7 @@ function onWingClick() {
 		isWingShow = true;
 		$(".wing-wrap").css("display", "block");
 		setTimeout(function(){
-			$(".wing-wrap").css("background-color", "rgba(0,0,0,0.4)");
+			$(".wing-wrap").css("background-color", "rgba(0, 0, 0, 0.4)");
 		}, 0);
 		setTimeout(function() {
 			$(".wing-conts").css("transform", "translateX(0)");
@@ -36,19 +65,55 @@ function onWingClick() {
 
 function onResize() {
 	// mobile -> pc
-	if( $(this).outerWidth() >= 768) {
+	if($(this).outerWidth()	>= 768) {
 		isWingShow = true;
 		onWingClick();
 	}
 
 	// pc -> mobile
 	if($(this).outerWidth() < 768) {
-
 		
 	}
 }
 
+function onScroll() {
+	/*
+	scTop = $(this).scrollTop();
+	$(".header").css("background-color", "white");
+	if(scTop > 200) $(".header").css("background-color", "beige");
+	if(scTop > 1000) $(".header").css("background-color", "orange");
+	if(scTop > 2000) $(".header").css("background-color", "red");
+	*/
+}
 
-/************* 이벤트등록*****************/
+function onMainPrev() {
+	mainNow = (mainNow == 0) ? mainLast : mainNow - 1;
+	mainAni();
+}
+
+function onMainNext() {
+	mainNow = (mainNow == mainLast) ? 0 : mainNow + 1;
+	mainAni();
+}
+
+function onMainPager() {
+	mainNow = $(this).index();
+	mainAni();
+}
+
+function onMainHover() {
+	clearInterval(mainInterval);
+}
+
+function onMainLeave() {
+	mainInterval = setInterval(onMainNext, mainGap);
+}
+
+/*********** 이벤트등록 ***********/
 $(".bt-wing").click(onWingClick);
 $(window).resize(onResize);
+$(window).scroll(onScroll);
+
+$(".main-wrap .bt-prev").click(onMainPrev);
+$(".main-wrap .bt-next").click(onMainNext);
+$(".main-wrap").hover(onMainHover, onMainLeave);
